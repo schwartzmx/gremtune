d77873fe61b3cef25f0a3783412757b3b9dee5ce
Thomas Obenaus <obenaus.thomas@gmail.com>
2020-02-05 15:15:21 +0100

    [#3] .Add added simple example

--- README.md
+++ README.md
@@ -171,6 +171,11 @@ func main() {
 
 This implementation supports [Graphson 2.0](http://tinkerpop.apache.org/docs/3.4.4/dev/io/#graphson-2d0) (not 3) in order to be compatible to CosmosDB. This means all the responses from the CosmosDB server as well the responses from the local gremlin-server have the 2.0 format.
 
+## Examples
+
+Examples can be found at [examples/](examples/).
+To run it on can use: `go run ./examples`
+
 ## License
 
 See [LICENSE](LICENSE.md)
---
+++ examples/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/schwartzmx/gremtune"
+)
+
+var panicOnErrorOnChannel = func(errs chan error) {
+	err := <-errs
+	if err == nil {
+		return // ignore if the channel was closed
+	}
+	log.Fatalf("Lost connection to the database: %s", err)
+}
+
+func main() {
+
+	host := "localhost"
+	port := 8182
+	hostURL := fmt.Sprintf("ws://%s:%d", host, port)
+
+	errs := make(chan error)
+	go panicOnErrorOnChannel(errs)
+
+	dialer := gremtune.NewDialer(hostURL)             // Returns a WebSocket dialer to connect to Gremlin Server
+	gremlinClient, err := gremtune.Dial(dialer, errs) // Returns a gremtune client to interact with
+	if err != nil {
+		log.Fatalf("Failed to create the gremlin client: %s", err)
+	}
+
+	// Sends a query to Gremlin Server
+	res, err := gremlinClient.Execute("g.V()")
+	if err != nil {
+		log.Fatalf("Failed to execute a gremlin command: %s", err)
+	}
+
+	jsonEncodedResponse, err := json.MarshalIndent(res[0].Result.Data, "", "    ")
+	if err != nil {
+		log.Fatalf("Failed to encode the raw json into json: %s", err)
+	}
+
+	log.Printf("Received data: \n%s\n", jsonEncodedResponse)
+}
