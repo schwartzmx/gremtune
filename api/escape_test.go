d34cab111c66a294ba0d334d3ace8273472e76b4
Thomas Obenaus <obenaus.thomas@gmail.com>
2020-03-31 14:10:31 +0200

    .Update escape values

--- api/escape_test.go
+++ api/escape_test.go
@@ -1,166 +1,45 @@
 package api
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestShouldEncodeTrue(t *testing.T) {
-	// GIVEN
-	value1 := "\n"
-	value2 := "a\na"
-	value3 := "a\n"
-	value4 := "\na"
-	value5 := "$"
-	value6 := "a$a"
-	value7 := "a$"
-	value8 := "$a"
-	value9 := `\`
-	value10 := `a\a`
-	value11 := `a\`
-	value12 := `\a`
-	value13 := "\t"
-	value14 := "\f"
-	value15 := "\r"
-	value16 := "\v"
-
-	// WHEN
-	should1 := shouldEncode(value1)
-	should2 := shouldEncode(value2)
-	should3 := shouldEncode(value3)
-	should4 := shouldEncode(value4)
-	should5 := shouldEncode(value5)
-	should6 := shouldEncode(value6)
-	should7 := shouldEncode(value7)
-	should8 := shouldEncode(value8)
-	should9 := shouldEncode(value9)
-	should10 := shouldEncode(value10)
-	should11 := shouldEncode(value11)
-	should12 := shouldEncode(value12)
-	should13 := shouldEncode(value13)
-	should14 := shouldEncode(value14)
-	should15 := shouldEncode(value15)
-	should16 := shouldEncode(value16)
-
-	// THEN
-	assert.True(t, should1)
-	assert.True(t, should2)
-	assert.True(t, should3)
-	assert.True(t, should4)
-	assert.True(t, should5)
-	assert.True(t, should6)
-	assert.True(t, should7)
-	assert.True(t, should8)
-	assert.True(t, should9)
-	assert.True(t, should10)
-	assert.True(t, should11)
-	assert.True(t, should12)
-	assert.True(t, should13)
-	assert.True(t, should14)
-	assert.True(t, should15)
-	assert.True(t, should16)
-}
-
-func TestShouldEncodeFalse(t *testing.T) {
-	// GIVEN
-	value1 := `\$`
-	value2 := `a\$a`
-	value3 := `\$a`
-	value4 := `a\$`
-	value5 := `\\`
-	value6 := `a\\a`
-	value7 := `a\\`
-	value8 := `\\a`
-	value9 := `\\`
-
-	// WHEN
-	should1 := shouldEncode(value1)
-	should2 := shouldEncode(value2)
-	should3 := shouldEncode(value3)
-	should4 := shouldEncode(value4)
-	should5 := shouldEncode(value5)
-	should6 := shouldEncode(value6)
-	should7 := shouldEncode(value7)
-	should8 := shouldEncode(value8)
-	should9 := shouldEncode(value9)
-
-	// THEN
-	assert.False(t, should1)
-	assert.False(t, should2)
-	assert.False(t, should3)
-	assert.False(t, should4)
-	assert.False(t, should5)
-	assert.False(t, should6)
-	assert.False(t, should7)
-	assert.False(t, should8)
-	assert.False(t, should9)
-}
-
 func TestEscape(t *testing.T) {
 	// GIVEN
-	value1 := "A\n\t\f\r\vB"
-	value2 := "A\\B"
-	value3 := "A$B"
-	//	value4 := "A\\$B"
+	value1 := "abcdefghijklmnopqrstufvxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+	value2 := `^°!"§$%&/()=?*+'#~,.;:-_<>|@€²³¼½¬{[]}\`
+
+	// WHEN
+	escaped1 := Escape(value1)
+	escaped2 := Escape(value2)
+
+	// THEN
+	assert.Equal(t, value1, escaped1)
+	assert.Equal(t, "%5E%C2%B0%21%22%C2%A7%24%25%26%2F%28%29%3D%3F%2A%2B%27%23~%2C.%3B%3A-_%3C%3E%7C%40%E2%82%AC%C2%B2%C2%B3%C2%BC%C2%BD%C2%AC%7B%5B%5D%7D%5C", escaped2)
+}
+
+func TestEscapeUnescape(t *testing.T) {
+	// GIVEN
+	value1 := "abcdefghijklmnopqrstufvxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+	value2 := `^°!"§$%&/()=?*+'#~,.;:-_<>|@€²³¼½¬{[]}\`
+	value3 := "Hello $'World"
 
 	// WHEN
 	escaped1 := Escape(value1)
 	escaped2 := Escape(value2)
 	escaped3 := Escape(value3)
-	//	escaped4 := Escape(value4)
+
+	unescaped1, err := UnEscape(escaped1)
+	assert.NoError(t, err)
+	unescaped2, err := UnEscape(escaped2)
+	assert.NoError(t, err)
+	unescaped3, err := UnEscape(escaped3)
+	assert.NoError(t, err)
 
 	// THEN
-	assert.Equal(t, "A%0A%09%0C%0D%0BB", escaped1)
-	assert.Equal(t, "A%5CB", escaped2)
-	assert.Equal(t, "A%24B", escaped3)
-	//	assert.Equal(t, "A\\$B", escaped4)
-}
-
-func TestUnEscape(t *testing.T) {
-	// GIVEN
-	value1 := "A\n\t\f\r\vB"
-	value2 := "A\\B"
-	value3 := "A$B"
-	//	value4 := "A\\$B"
-
-	// WHEN
-	escaped1 := Escape(value1)
-	escaped2 := Escape(value2)
-	escaped3 := Escape(value3)
-	//	escaped4 := Escape(value4)
-
-	// THEN
-	assert.Equal(t, "A%0A%09%0C%0D%0BB", escaped1)
-	assert.Equal(t, "A%5CB", escaped2)
-	assert.Equal(t, "A%24B", escaped3)
-	//	assert.Equal(t, "A\\$B", escaped4)
-}
-
-func TestEscapeDollar(t *testing.T) {
-	// GIVEN
-	value1 := "$"
-	value2 := "\\$"
-
-	// WHEN
-	escaped1 := EscapeGroovyChars(value1)
-	escaped2 := EscapeGroovyChars(value2)
-
-	// THEN
-	assert.Equal(t, "\\$", escaped1)
-	assert.Equal(t, "\\$", escaped2)
-}
-
-func TestUnEscapeDollar(t *testing.T) {
-	// GIVEN
-	value := "A$B\\$C\\"
-	escaped := EscapeGroovyChars(value)
-	// WHEN
-	unescaped := UnEscapeGroovyChars(escaped)
-	fmt.Printf("%s -> %s -> %s\n", value, escaped, unescaped)
-
-	// THEN
-	assert.Equal(t, value, unescaped)
-	assert.Fail(t, "failureMessage")
+	assert.Equal(t, value1, unescaped1)
+	assert.Equal(t, value2, unescaped2)
+	assert.Equal(t, value3, unescaped3)
 }
