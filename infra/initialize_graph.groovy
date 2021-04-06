8ba67620f1ef2e30ba074453d065ebab59781dc3
Thomas Obenaus <obenaus.thomas@gmail.com>
2021-04-06 13:22:39 +0200

    [#56] .Update ensure that props are returned

---
+++ infra/initialize_graph.groovy
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// An example of an initialization script that can be configured to run in Gremlin Server.
+// Functions defined here will go into global cache and will not be removed from there
+// unless there is a reset of the ScriptEngine.
+def addItUp(x, y) { x + y }
+
+// an init script that returns a Map allows explicit setting of global bindings.
+def globals = [:]
+
+// defines a sample LifeCycleHook that prints some output to the Gremlin Server console.
+// note that the name of the key in the "global" map is unimportant.
+globals << [hook : [
+  onStartUp: { ctx ->
+    ctx.logger.info("Starting initialize_graph.groovy. Executed once at startup of Gremlin Server.")
+  },
+  onShutDown: { ctx ->
+    ctx.logger.info("Executed once at shutdown of Gremlin Server.")
+  }
+] as LifeCycleHook]
+
+// Don't use ReferenceElementStrategy in order to ensure to be compatible with the cosmos DB implementation.
+// CosmosDB returns vertices and edges always WITH their properties. 
+// If the ReferenceElementStrategy would be used the vertices and edges would be returned WITHOUT their properties.
+// See: https://stackoverflow.com/questions/55388014/gremlin-tinkerpop-propertymap-has-values-but-vertex-properties-is-empty
+// See: https://tinkerpop.apache.org/docs/current/reference/#_properties_of_elements
+globals << [g : graph.traversal()]
